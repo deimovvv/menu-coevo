fe3aca3074c97b7642cf6e70ae26321d4db82fe3
deimov <cortezgonzalo@outlook.es>
2024-01-22 23:59:50 -0300

    aaajj

--- src/components/three.js/Experience.tsx
+++ src/components/three.js/Experience.tsx
@@ -1,5 +1,5 @@
 import { OrbitControls, Environment } from '@react-three/drei';
-import { Canvas ,useLoader} from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 /* import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader' */
 import { Model } from './Model';
 

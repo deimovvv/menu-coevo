ab4ca62c8109556119c02edbbaccbb2b5c19e62a
deimov <cortezgonzalo@outlook.es>
2024-02-19 16:34:07 -0300

    jjdddd

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import {BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import Navbar from "./components/layout/Navbar/Navbar";
-import Experience from "./components/three.js/Experience";
+
 
 const App = () => {
 
